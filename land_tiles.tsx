f23658fae071367f496a043bebe90aff77ab6e10
TomekoLeko <ays@vp.pl>
2023-05-09 11:55:06 +0200

    new map

---
+++ land_tiles.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="land_map" tilewidth="16" tileheight="16" spacing="1" margin="1" tilecount="5112" columns="36">
+ <image source="../phaser-playground/land.png" trans="ff00ff" width="613" height="2415"/>
+</tileset>
